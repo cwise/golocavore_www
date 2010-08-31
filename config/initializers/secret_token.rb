641ecd5ee08fc97b2fa4c4b8d3ac60e6e5f340b0
Chris Wise <cwise@murmurinformatics.com>
2010-08-31 09:20:23 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GolocavoreWww::Application.config.secret_token = 'c63fa5f753f6c17ca49a541b6139c4ec31c65d571ada12ed39380a6962d4ff68126e2a5e7650c8689ad9f97c9cd7241363b100095b0cf88b2c90dfc004b64563'
